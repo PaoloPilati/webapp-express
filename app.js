dc0e15cb00b88e9712e4ea71c5355ffb30201a13
Zizou10 <paolo.pilati93@gmail.com>
2026-05-04 00:21:05 +0200

    Creazione schema, connessione al db e creazione API Index

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const db = require('./db');
 const app = express();
 
 const port = 3000;
+
+// INDEX
+app.get('/movies', (req, res) => {
+  db.query('SELECT * FROM movies', (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    res.json(results);
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Server in ascolto su http://localhost:${port}`);
--- db.js
+++ db.js
@@ -7,4 +7,12 @@ const connection = mysql.createConnection({
   database: 'movies_db'
 });
 
+connection.connect((err) => {
+  if (err) {
+    console.error('Errore di connessione:', err);
+    return;
+  }
+  console.log('Connesso al db!');
+});
+
 module.exports = connection;
