ca08b63c0d6dbac96e3e640033e89e76d127f380
Zizou10 <paolo.pilati93@gmail.com>
2026-05-04 14:25:37 +0200

    Troubleshooting controller + rename cartella img---> public

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.json());
 // routes
 app.use('/movies', moviesRoutes);
 
+//middleware per le immagini
+app.use(express.static('public'));
+
 app.listen(3000, () => {
   console.log('Server attivo su http://localhost:3000');
 });
--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,19 +1,25 @@
 const db = require('../db/connection');
 
+
 // INDEX
-app.get('/movies', (req, res) => {
+function index(req, res) {
   db.query('SELECT * FROM movies', (err, results) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
 
+    results.forEach(movie => {
+      movie.image_url = getImageUrl(movie.image);
+    });
+
     res.json(results);
   });
-});
+}
 
-//SHOW (CON NESTING JSON)
-app.get('/movies/:id', (req, res) => {
+// SHOW
+function show(req, res) {
   const id = req.params.id;
+
   const movieSql = 'SELECT * FROM movies WHERE id = ?';
   const reviewsSql = 'SELECT * FROM reviews WHERE movie_id = ?';
 
@@ -21,11 +27,13 @@ app.get('/movies/:id', (req, res) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
+
     if (movieResults.length === 0) {
       return res.status(404).json({ error: 'Movie not found' });
     }
 
     const movie = movieResults[0];
+    movie.image_url = getImageUrl(movie.image);
 
     db.query(reviewsSql, [id], (err, reviewsResults) => {
       if (err) {
@@ -37,10 +45,10 @@ app.get('/movies/:id', (req, res) => {
       res.json(movie);
     });
   });
-});
+}
 
 
 
 
 
-module.export {index, show};
+module.exports = { index, show };
