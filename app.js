2a580f802f49d3bd40e2494b46b398a3c51cbe8a
Zizou10 <paolo.pilati93@gmail.com>
2026-05-10 21:53:17 +0200

    Installazione + import cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const notFound = require('./middlewares/notFound');
 const errorHandler = require('./middlewares/errorHandler');
